a2903fa43909e0bb77d1d2a9751795e3f0893bfc
dsapr <785722840@qq.com>
2022-02-08 23:07:39 +0800

    first commit

---
+++ src/main/java/com/dsapr/dsaprmusic/DsaprMusicApplication.java
@@ -0,0 +1,13 @@
+package com.dsapr.dsaprmusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsaprMusicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DsaprMusicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dsapr/dsaprmusic/DsaprMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsapr.dsaprmusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsaprMusicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
